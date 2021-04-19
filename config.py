105322749989cb94720dedd491d7b236a2ceaa76
Olav Schettler <olav@schettler.net>
2021-04-19 09:18:20 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------
+# Site Configuration File
+# ------------------------------------------------------------------------------
+
+# Choose the theme to use when building your site. This variable should specify
+# the name of a theme directory in your site's 'lib' folder.
+theme = "carbon"
+
+title = "olav.net"
+
+tagline = "digital content. gadgets. life."
+
+extensions = ['holly']
+
+holly = {
+    "roots": [
+        {"root_url": "@root/blog//"},
+    ]
+}
+
