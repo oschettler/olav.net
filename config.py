cdf4083cffa7a92dbeb8fcf03db2987f9d78302f
Olav Schettler <olav.schettler@dw.com>
2024-11-11 17:47:11 +0100

    Comment added

--- config.py
+++ config.py
@@ -4,6 +4,8 @@
 
 # Choose the theme to use when building your site. This variable should specify
 # the name of a theme directory in your site's 'lib' folder.
+# 2024-11-11 This now runs with Ark, no longer Ivy
+
 theme = "carbon"
 
 title = "olav.net"
